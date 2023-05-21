2793efe4138d1ba69ff7944fa9ab5a0859859f6a
kozraf <52557563+kozraf@users.noreply.github.com>
2023-05-21 17:37:37 +0100

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -3,6 +3,11 @@ output "bucket_id" {
   value       = aws_s3_bucket.tfstate.id
 }
 
+output "bucket_name" {
+  description = "The name of the bucket"
+  value       = aws_s3_bucket.tfstate.bucket_domain_name
+}
+
 output "dynamodb_table_name" {
   description = "The name of the DynamoDB table"
   value       = aws_dynamodb_table.tf_lock.name
