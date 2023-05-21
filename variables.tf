e944054ecb249dc3174fc11249afc3ca4b97d904
kozraf <52557563+kozraf@users.noreply.github.com>
2023-05-21 15:18:19 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "aws_access_key" {
   description = "aws_access_key"
   type        = string
-  default     = AKIAWPVNWZXHCW6CNOXH
 }
 
 variable "aws_secret_key" {
   description = "aws_secret_key"
   type        = string
-  default     = Fhnsasv36KpgwoafMIcHJZXkE9T3Z3GDkAX1r4x/
 }
