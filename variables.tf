5834ff41fe92ba74bca45a95287b08c8dfc97803
kozraf <52557563+kozraf@users.noreply.github.com>
2023-05-21 14:56:42 +0100

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "aws_access_key" {
-  description = "AWS access key"
+  description = "AKIAWPVNWZXHCW6CNOXH"
   type        = string
 }
 
 variable "aws_secret_key" {
-  description = "AWS secret key"
+  description = "Fhnsasv36KpgwoafMIcHJZXkE9T3Z3GDkAX1r4x/"
   type        = string
 }
