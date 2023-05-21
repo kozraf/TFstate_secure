8474bfd2b9bcc1b5b42b12f1fbd3caaf1875eb0a
kozraf <52557563+kozraf@users.noreply.github.com>
2023-05-21 20:15:09 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region  = "us-west-2"
-  #access_key = var.aws_access_key
-  #secret_key = var.aws_secret_key
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
 }
 
 resource "random_pet" "bucket_name" {
